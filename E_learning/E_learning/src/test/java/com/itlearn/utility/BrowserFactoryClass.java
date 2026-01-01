edae064a6e73e8e5e6ff25dfdfeaaa7955bcd188
aditya1994-lgtm <67704804+aditya1994-lgtm@users.noreply.github.com>
2026-01-01 13:12:07 +0530

    Update BrowserFactoryClass.java

--- E_learning/E_learning/src/test/java/com/itlearn/utility/BrowserFactoryClass.java
+++ E_learning/E_learning/src/test/java/com/itlearn/utility/BrowserFactoryClass.java
@@ -1,38 +1,40 @@
 package com.itlearn.utility;
 
 import java.time.Duration;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BrowserFactoryClass {
 
+    // Start browser and open URL
     public static WebDriver startApplication(WebDriver driver, String browserName, String appUrl) {
-
         if (browserName.equalsIgnoreCase("chrome")) {
-            WebDriverManager.chromedriver().setup();
+            WebDriverManager.chromedriver().setup(); // auto-download correct version
             driver = new ChromeDriver();
-        }
-        else if (browserName.equalsIgnoreCase("firefox")) {
+        } else if (browserName.equalsIgnoreCase("firefox")) {
             WebDriverManager.firefoxdriver().setup();
             driver = new FirefoxDriver();
-        }
-        else if (browserName.equalsIgnoreCase("ie")) {
+        } else if (browserName.equalsIgnoreCase("IE")) {
             WebDriverManager.iedriver().setup();
             driver = new InternetExplorerDriver();
-        }
-        else {
-            throw new RuntimeException("Browser not supported: " + browserName);
+        } else {
+            System.out.println("Browser not supported: " + browserName);
         }
 
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get(appUrl);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         return driver;
+    }
+
+    // Close browser safely
+    public static void quitBrowser(WebDriver driver) {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
