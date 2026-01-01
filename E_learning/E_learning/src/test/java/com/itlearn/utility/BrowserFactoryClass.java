afb5070ec1348192cdcf132134e36897f974beae
aditya1994-lgtm <67704804+aditya1994-lgtm@users.noreply.github.com>
2026-01-01 13:08:07 +0530

    Update BrowserFactoryClass.java

--- E_learning/E_learning/src/test/java/com/itlearn/utility/BrowserFactoryClass.java
+++ E_learning/E_learning/src/test/java/com/itlearn/utility/BrowserFactoryClass.java
@@ -3,45 +3,44 @@ package com.itlearn.utility;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BrowserFactoryClass {
-	public static WebDriver startApplication(WebDriver driver,String browserName,String appUrl)
-	{
-		
-		   if (browserName.equalsIgnoreCase("Chrome")) {
-		        System.setProperty("webdriver.chrome.driver", "C:\\BrowserDriver\\chromedriver.exe");
-		        driver = new ChromeDriver();
-		    }
-		else if(browserName.equals("Firefox"))
-		{
-			System.setProperty("webdriver.firefox.driver", "./Drivers/chromedriver.exe");
-	         driver = new FirefoxDriver();
-		}
-		else if(browserName.equals("IE"))
-		{
-			System.setProperty("webdriver.edge.driver", "./Drivers/chromedriver.exe");
-	         driver = new InternetExplorerDriver();
-		}
-		else
-		{
-			System.out.println("We do not support this browser ");
-		}
 
-		
-		driver.manage().window().maximize();
-		driver.get(appUrl);
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-	
-		return driver;
+    public static WebDriver startApplication(WebDriver driver, String browserName, String appUrl) {
 
-	}
+        if (browserName.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();   // Auto matches Chrome version
+            driver = new ChromeDriver();
+        }
+        else if (browserName.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+        else if (browserName.equalsIgnoreCase("ie")) {
+            WebDriverManager.iedriver().setup();
+            driver = new InternetExplorerDriver();
+        }
+        else {
+            throw new RuntimeException("Browser not supported: " + browserName);
+        }
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(appUrl);
+
+        return driver;
+    }
+}
+
 	
 	public static void quitBrowser(WebDriver driver)
 	{
 		driver.quit();
 	}
 }
+
